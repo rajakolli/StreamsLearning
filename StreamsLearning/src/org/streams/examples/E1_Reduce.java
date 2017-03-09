85ce7ef066ce579bd5ea1c4a6c5560cd176400da
GAMESYS\raja.kolli <raja.kolli@gamesys.co.uk>
2017-03-09 13:57:51 +0000

    code added to learn Reduce (sum)

--- StreamsLearning/src/org/streams/examples/E1_Reduce.java
+++ StreamsLearning/src/org/streams/examples/E1_Reduce.java
@@ -1,9 +1,34 @@
 package org.streams.examples;
 
+import java.util.Arrays;
+import java.util.List;
+
+/*
+ * The reduce() operation combines all elements in a stream to produce a single value.
+ * The reduce operation takes two parameters called a seed (initial value) and an accumulator.
+ * The accumulator is a function. If the stream is empty, the seed is the result.
+ * The seed and an element are passed to the accumulator, which returns partial result. 
+ * 			And then the partial result and the next element are passed to the accumulator function.
+ * This repeats until all elements are passed to the accumulator. 
+ * 			The last value returned from the accumulator is the result of the reduce operation.
+ * The stream-related interfaces contain two methods called reduce() and collect() to perform generic reduce operations.
+ * Methods such as sum(), max(), min(), count(), 
+ * 			are defined in IntStream, LongStream, and DoubleStream interfaces.
+ * count() method is available for all types of streams.
+ * The Stream<T> interface contains a reduce() method to perform the reduce operation. 
+ * 			The method has three overloaded versions:
+ * 				T  reduce(T identity, BinaryOperator<T> accumulator)
+ * 				<U> U reduce(U identity, BiFunction<U,? super  T,U> accumulator, BinaryOperator<U> combiner)
+ * 				Optional<T> reduce(BinaryOperator<T> accumulator)
+ */
 public class E1_Reduce {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		List<Integer> numbers = Arrays.asList(1,2,3,4,5);
+		int sum = numbers.stream()
+						.reduce(0,  Integer::sum);
+		
+		System.out.println(sum);
 
 	}
 
