fb21f565701e5c7a26be9dfe57e1380875aec76b
rajakolli <rajakolli7@gmail.com>
2017-03-09 17:18:45 +0000

    calculates the sum of the incomes

---
+++ StreamsLearning/src/org/streams/examples/F1_Aggregation.java
@@ -0,0 +1,21 @@
+package org.streams.examples;
+/*
+ * To compute the sum, max, min, average, etc. on a numeric stream, 
+ * 		we can map the non-numeric stream into numeric stream types (IntStream, LongStream, or DoubleStream) 
+ * 				and then use the specialized methods.
+ */
+public class F1_Aggregation {
+	/*
+	 * The following code calculates the sum of the incomes. 
+	 * 		mapToDouble() method converts a Stream<Employee> to a DoubleStream. 
+	 * 		The sum() method is called on the DoubleStream.
+	 */
+	public static void main(String[] args) {
+		double	totalIncome = Employee.persons()
+							.stream()
+							.mapToDouble(Employee::getIncome)
+							.sum();
+
+	}
+
+}
