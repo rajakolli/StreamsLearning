e8ea11a3d074944c60c5542808a7359e151afd4b
rajakolli <rajakolli7@gmail.com>
2017-03-09 17:31:02 +0000

    To get the minimum and maximum values of a stream, use the min() and max() methods of the specific stream.

---
+++ StreamsLearning/src/org/streams/examples/F2_Aggregation.java
@@ -0,0 +1,26 @@
+package org.streams.examples;
+
+import java.util.Comparator;
+import java.util.Optional;
+
+/*
+ * To compute the sum, max, min, average, etc. on a numeric stream, 
+ * 		we can map the non-numeric stream into numeric stream types (IntStream, LongStream, or DoubleStream) 
+ * 				and then use the specialized methods.
+ */
+public class F2_Aggregation {
+	/*
+	 * To get the minimum and maximum values of a stream, 
+	 * 			use the min() and max() methods of the specific stream.
+	 */
+	public static void main(String[] args) {
+		Optional<Employee> person = Employee.persons().stream()
+				.max(Comparator.comparing(Employee::getIncome));
+		
+		if (person.isPresent()) {
+			System.out.println("Highest earner: " + person.get());
+		} else {
+			System.out.println("Could not get the highest earner.");
+		}
+	}
+}
